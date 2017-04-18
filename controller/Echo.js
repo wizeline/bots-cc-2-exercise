4e702e241cdbe799b8373620ba57f9ba973f3d53
Angel Malavar <angel.malavar@gmail.com>
2017-04-18 15:28:24 -0500

    Linted issues

--- controller/Echo.js
+++ controller/Echo.js
@@ -1,18 +1,17 @@
-const sender = require('./Sender');
-
 // Compose the facebook echo message object to our
 class Echo {
-  composeMessage(messagingEvent){
-    if (messagingEvent.message && messagingEvent.message.is_echo) { // Subscribes to Message Echo Callback
+  composeMessage(messagingEvent) {
+    if (messagingEvent.message && messagingEvent.message.is_echo) {
+      // Subscribes to Message Echo Callback
       // Indicates the message was sent from the page itself
       return [];
     }
-    const message = messagingEvent.message;
-    return [this.composeTextMessage(message)];
+    this.message = messagingEvent.message;
+    return [this.composeTextMessage()];
   }
-  composeTextMessage(message) {
+  composeTextMessage() {
     return {
-      text: message.text
+      text: this.message.text,
     };
   }
 }
